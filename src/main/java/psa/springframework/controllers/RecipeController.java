b51ceffa4f262f7f218773a8a4e14586554babbf
Pyae Sone Aung <pyaesone1511@icloud.com>
2021-01-17 14:09:09 +0630

    feat : Create a Recipe

--- src/main/java/psa/springframework/controllers/RecipeController.java
+++ src/main/java/psa/springframework/controllers/RecipeController.java
@@ -2,8 +2,8 @@ package psa.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import psa.springframework.commands.RecipeCommand;
 import psa.springframework.services.RecipeService;
 
 @Controller
@@ -19,6 +19,21 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe",recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe",new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/"+ savedCommand.getId();
     }
 
 }
