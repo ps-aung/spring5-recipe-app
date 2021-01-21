0beab13cd20b0561a4a4d07e69044e7815a1bb3f
Pyae Sone Aung <pyaesone1511@icloud.com>
2021-01-21 21:20:41 +0630

    feat : uploading a image

---
+++ src/main/java/psa/springframework/services/ImageService.java
@@ -0,0 +1,8 @@
+package psa.springframework.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile imageFile);
+}
